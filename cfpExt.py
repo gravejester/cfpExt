fa7b6437a4dc65fcd64bed22ab10294c52488ea8
Øyvind Kallstad <kallstad@gmail.com>
2014-03-17 18:23:31 +0100

    Added handling of is_enabled for menu

--- cfpExt.py
+++ cfpExt.py
@@ -1,9 +1,17 @@
 import sublime, sublime_plugin, os
 
 class FilenameToClipboardCommand(sublime_plugin.TextCommand):
-   def run(self, edit):
-      sublime.set_clipboard(os.path.basename(self.view.file_name()))
+	def run(self, edit):
+		sublime.set_clipboard(os.path.basename(self.view.file_name()))
+		sublime.status_message("Copied filename")
+
+	def is_enabled(self):
+		return self.view.file_name() and len(self.view.file_name()) > 0
 
 class PathToClipboardCommand(sublime_plugin.TextCommand):
-   def run(self, edit):
-      sublime.set_clipboard(os.path.dirname(self.view.file_name()))
+	def run(self, edit):
+		sublime.set_clipboard(os.path.dirname(self.view.file_name()))
+		sublime.status_message("Copied path")
+
+	def is_enabled(self):
+		return self.view.file_name() and len(self.view.file_name()) > 0
